eb135afde7bd7c5047ab9efea30918cf2713ebb3
sknight <shawn.knight@lifeway.com>
2016-07-30 21:44:27 -0500

    initial commit

---
+++ 08-2016/WebDriverInitialization/code/WebDriverHelperIfStatement.java
@@ -0,0 +1,36 @@
+package ?;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverHelperIfStatement
+{
+    public WebDriver initDriver(String browser) {
+
+        WebDriver driver;
+
+        if ("chrome".equalsIgnoreCase(browser)) {
+            ClassLoader classLoader = getClass().getClassLoader();
+            System.setProperty("webdriver.chrome.driver", classLoader.getResource("chromedriver.exe").getPath());
+            driver = new ChromeDriver();
+        }
+
+        else if ("firefox".equalsIgnoreCase(browser)) {
+            driver = new FirefoxDriver();
+        }
+
+        else if ("safari".equalsIgnoreCase(browser)) {
+            driver = new SafariDriver();
+        }
+
+        else {
+            driver = new HtmlUnitDriver();
+        }
+
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
